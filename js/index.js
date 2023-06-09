8cc0f1cb7957390dcfdd5fef55fbd535368273c7
AdrianRgGit <adrianramirezgalera@gmail.com>
2023-06-09 09:13:26 +0200

    Terminada la primera versión

--- js/index.js
+++ js/index.js
@@ -2,7 +2,9 @@
 const nameInputSelector = document.querySelector("#name-input");
 const emailInputSelector = document.querySelector("#email-input");
 const passwordInputSelector = document.querySelector("#password-input");
-const confirmPasswordInputSelector = document.querySelector("#confirm-password-input");
+const confirmPasswordInputSelector = document.querySelector(
+  "#confirm-password-input"
+);
 const buttonInputSelector = document.querySelector("#button-input");
 const parrafoErrorSelector = document.querySelector("#p-error");
 const parrafoExitoSelector = document.querySelector("#p-exito");
@@ -12,8 +14,13 @@ const cardEmailSelector = document.querySelector("#email-card");
 let credenciales = {};
 
 // Función guardar usuario
+// He separado los tiempos porque cada vez que le dábamos a enviar y el usuario no era correcto te llevaba a la pantalla de usuario.
 function guardarUsuario() {
   localStorage.setItem(credenciales.email, JSON.stringify(credenciales));
+  parrafoExitoSelector.innerHTML = ""; // Faltaba el parrafoExito para que desaparezca a los 3 seg
+  setTimeout(function () {
+    window.location.href = document.getElementById("pageLink").href; // Propiedad que devuelve la url en la que está el script
+  }, 3000);
   console.log(credenciales);
 }
 
@@ -35,7 +42,7 @@ function validarCredenciales(event) {
     confirmPasswordInputSelector.value === ""
   ) {
     parrafoExitoSelector.innerHTML = "";
-    parrafoErrorSelector.innerHTML = `<p class="alert alert-danger">"Las contraseñas no coinciden"</p>`;
+    parrafoErrorSelector.innerHTML = `<p class="alert alert-danger">"Rellene los campos"</p>`;
   }
 
   //   Validación email
@@ -43,14 +50,14 @@ function validarCredenciales(event) {
     /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/.test(emailInputSelector.value) !== true
   ) {
     parrafoExitoSelector.innerHTML = "";
-    parrafoErrorSelector.innerHTML = `<p class="alert alert-danger">"Las contraseñas no coinciden"</p>`;
+    parrafoErrorSelector.innerHTML = `<p class="alert alert-danger">"Correo no válido"</p>`;
   }
-  
-    //   Validación contraseña
-    else if (/^[a-zA-Z]\w{3,14}$/.test(passwordInputSelector.value)) {
-      parrafoExitoSelector.innerHTML = "";
-      parrafoErrorSelector.innerHTML = `<p class="alert alert-danger">"Las contraseñas no coinciden"</p>`;
-    }
+
+  //   Validación contraseña
+  else if (/^[a-zA-Z]\w{3,14}$/.test(passwordInputSelector.value) !== true) {
+    parrafoExitoSelector.innerHTML = "";
+    parrafoErrorSelector.innerHTML = `<p class="alert alert-danger">"Contraseña no válida"</p>`;
+  }
 
   //   Validación Contraseñas iguales
   else if (passwordInputSelector.value !== confirmPasswordInputSelector.value) {
@@ -68,10 +75,7 @@ function validarCredenciales(event) {
   // Borrar mensaje
   setTimeout(function () {
     parrafoErrorSelector.innerHTML = "";
-    parrafoExitoSelector.innerHTML = ""; // Faltaba el parrafoExito para que desaparezca a los 3 seg
-    window.location.href = document.getElementById("pageLink").href; // Propiedad que devuelve la url en la que está el script
   }, 3000);
-
 }
 
 buttonInputSelector.addEventListener("click", validarCredenciales);
